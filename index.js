d88f5bc49b6a081dc17f7e50754df8031e5a2fe9
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-08-30 17:23:01 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+//this is a js file
+// joao paulo
+//function imprimirolamundo()[console.log("ola mundo")]
+//imprimirolamundo();
+
+//function escrevenome(nome){
+  //   console.log(nome);}
+     
+//let meuNome = "joao";
+//escrevenome(meuNome);
+/*
+//const a = 1 
+//function imprimeVariavel () {
+ //const b = 2 
+ //console.log('Variavel a' , a)
+ //console.log('Variavel b' , b)
+}
+//imprimeVariavel()
+//console.log('Variavel a' , a)
+console.log( 'variavel b' , b)
+
+
+function calculaArea(altura, largura){
+     const area = altura * largura;
+     return area;
+}
+const areacalculada = calculaArea(5, 8);
+console.log("Variavel areacalculada: "+ areacalculada);
+console.log("chamada da função crua: " + calculaArea(5, 8));
+
+
+function somaValores(a,b){
+     return Number(a)+Number(b);
+}
+const resultadoDaSoma = somaValores(2,4);
+console.log("O resultado da soma é:"+ resultadoDaSoma);
+*/
+
+function dividearray(meuarray){
+ let b = [(meuarray[0] /2),
+ (meuarray[meuarray.length-1] /2)];
+ 
+return b;
+}
+let a = [1,2,3,4,5,6,7,8,9,10,11.12];
+const resultado = dividearray(a)
+console.log(resultado);
+
+
+
+
+
+
+
